c266ca9e134387eddfd8ef7f4af73c189951465a
brianbravoski28@gmail.com <brri>
2025-01-27 21:47:44 +0300

    setting the order correctly

--- nginx-log-analyzer.sh
+++ nginx-log-analyzer.sh
@@ -11,13 +11,13 @@ if [ ! -f $Log_File ]; then
     echo "Error: $Log_File not found."
     exit 1
 fi
-
+echo -----------------------------------------
 echo "Top 5 IP addresses in the log file: $Log_File"
-awk '{print $1}' $Log_File | sort | uniq -c | head -n 5 | sort -nr 
+awk '{print $1}' $Log_File | sort | uniq -c | head -n 5 | sort -nr | awk '{print $2,  " - "$1 " requests"}'
 echo ----------------------------------------
 echo ----------------------------------------
 echo "Top 5 URLs in the log file: $Log_File"
-awk '{print $7}' $Log_File | sort | uniq -c | sort -nr | head -n 5
+awk '{print $7}' $Log_File | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2,  " - "$1 " requests"}'
 echo ----------------------------------------
 
 echo ----------------------------------------
@@ -27,4 +27,4 @@ echo ----------------------------------------
 
 echo ----------------------------------------
 echo "Top 5 Response Codes in the log file: $Log_File"
-awk '{print $9}' $Log_File | sort | uniq -c | sort -nr | head -n 5
+awk '{print $9}' $Log_File | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2,  " - "$1 " requests"}'
