9c2e891a206df75e5e70351484c245f43d6d3a53
brianbravoski28@gmail.com <brri>
2025-01-27 21:23:21 +0300

    setup check

---
+++ nginx-log-analyzer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <nginx log file>"
+    exit 1
+fi
+
+Log_File=$1
+
+if [ ! -f $Log_File ]; then
+    echo "Error: $Log_File not found."
+    exit 1
+fi
+
+echo "Top 5 IP addresses in the log file: $Log_File"
+awk '{print $1}' $Log_File | sort | uniq -c | head -n 5 | sort -nr 
+echo ----------------------------------------
+echo ----------------------------------------
+echo "Top 5 URLs in the log file: $Log_File"
+awk '{print $7}' $Log_File | sort | uniq -c | sort -nr | head -n 5
+echo ----------------------------------------
+
+echo ----------------------------------------
+echo "Top 5 User Agents in the log file: $Log_File"
+awk -F\" '{print $6}' $Log_File | sort | uniq -c | sort -nr | head -n 5
+echo ----------------------------------------
+
+echo ----------------------------------------
+echo "Top 5 Response Codes in the log file: $Log_File"
+awk '{print $9}' $Log_File | sort | uniq -c | sort -nr | head -n 5
